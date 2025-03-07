9381159da7e86a1092bc82df4353efcf5ee178e3
Shawn 'Rocky' Gaston <pilamrocky@gmail.com>
2025-03-07 01:15:28 -0500

    Split Settings and Docker Working

--- vr_score_keeper/settings/settings/base.py
+++ vr_score_keeper/settings/settings/base.py
@@ -10,20 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-17cbnu!oqiz=@38d(+hi7lpw23rek^y7wswt80w9fq(!%5lx7&"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-your-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
---
+++ vr_score_keeper/settings/settings/dev.py
@@ -0,0 +1,6 @@
+# Import settings from base.py
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*", "127.0.0.1", "[::1]"]
