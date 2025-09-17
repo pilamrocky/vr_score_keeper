2db57159fa1a9d210de86f2de0dd0a95a9f5fa2f
Shawn 'Rocky' Gaston <pilamrocky@gmail.com>
2025-09-17 01:50:01 -0400

    Refactor has_group filter to simplify group existence check and improve readability

--- vr_score_keeper/vr_tournaments/templatetags/user_tags.py
+++ vr_score_keeper/vr_tournaments/templatetags/user_tags.py
@@ -6,8 +6,6 @@ register = template.Library()
 
 @register.filter(name="has_group")
 def has_group(user, group_name):
-    try:
-        group = Group.objects.get(name=group_name)
-    except Group.DoesNotExist:
-        return False
-    return group in user.groups.all() or user.is_superuser
+    if user.is_superuser:
+        return True
+    return user.groups.filter(name=group_name).exists()
